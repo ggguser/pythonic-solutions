5e3facef5a5c8405684206c2222ffed9e380317b
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-06 13:23:42 +0300

    Recovered the manual sorting function

--- app/sorting.py
+++ app/sorting.py
@@ -10,22 +10,18 @@ def frequency_sort_string_counter(input_string: str):
 
     return list(char for char, freq in counter.most_common())
 
-s = 'aabbbbbbccc'
 
-c = Counter(s)
-print(c)
-
-print(Counter(s).most_common())
-
-
-def frequency_sorting(elements: list):
+def frequency_sorting_manual(elements: list):
     """
     Reduce the list to unique elements and sort them by frequency
-    >>> frequency_sorting([1, 2, 1, 3, 3, 3, 3])
+    >>> frequency_sorting_manual([1, 2, 1, 3, 3, 3, 3])
     [3, 1, 2]
 
-    >>> frequency_sorting(['c', 'c', 'b', 'b', 'b', 'a'])
+    >>> frequency_sorting_manual(['c', 'c', 'b', 'b', 'b', 'a'])
     ['b', 'c', 'a']
+
+    >>> frequency_sorting_manual([3, 3, 1, 2, 1, 2])
+    [1, 2, 3]
     """
     #  Composing a dictionary with 'elements': frequency pairs
     frequency = {}
@@ -34,9 +30,34 @@ def frequency_sorting(elements: list):
         frequency[u] = elements.count(u)
 
     #  From dictionary composing a list of, sorted by frequency
-    sorted_frequency = sorted(frequency.items(), key=lambda value_to_sort: value_to_sort[1], reverse=True)
     result = []
-    for (key, value) in sorted_frequency:
+    for x in range(len(frequency)):
+        for key in frequency:
+            if frequency[key] == max(frequency.values()) and max(frequency.values()) != 0:
+                result.append(key)
+                frequency[key] = 0
+    return result
+
+
+def frequency_list_sorting_simplified(elements: list):
+    """
+    Reduce the list to unique elements and sort them by frequency
+    >>> frequency_list_sorting_simplified([1, 2, 1, 3, 3, 3, 3])
+    [3, 1, 2]
+
+    >>> frequency_list_sorting_simplified([1, 2, 1, 2, 3, 3])
+    [1, 2, 3]
+
+    >>> frequency_list_sorting_simplified(['c', 'c', 'b', 'b', 'b', 'a'])
+    ['b', 'c', 'a']
+    """
+    #  Composing a dictionary with 'elements': frequency pairs
+    frequency = Counter(elements)
+    #  From dictionary composing a list of tuples, sorted by frequency
+    sorted_frequency = frequency.most_common()
+    #  From that list getting only the keys
+    result = []
+    for key, value in sorted_frequency:
         result.append(key)
     return result
 
@@ -56,6 +77,3 @@ def frequency_string_sort(input_string):
         for i in range(v):
             output += k
     return output
-
-
-print(frequency_string_sort('aabbbbbbccc'))
