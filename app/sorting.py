40cc6c4ff9ee3f9ce838642ee9b22868f4633676
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-06 14:04:13 +0300

    Collected all the nice solutions

--- app/sorting.py
+++ app/sorting.py
@@ -1,13 +1,36 @@
 from collections import Counter
 
 
-def frequency_sort_string_counter(input_string: str):
+def frequency_list_sorting_with_counter(elements: list):
     """
-    >>> frequency_sort_string_counter('aabbbbbbccc')
+    Reduce the list to unique elements and sort them by frequency
+    >>> frequency_list_sorting_with_counter([1, 2, 1, 3, 3, 3, 3])
+    [3, 1, 2]
+
+    >>> frequency_list_sorting_with_counter([1, 2, 1, 2, 3, 3])
+    [1, 2, 3]
+
+    >>> frequency_list_sorting_with_counter(['c', 'c', 'b', 'b', 'b', 'a'])
     ['b', 'c', 'a']
     """
-    counter = Counter(input_string)
+    #  Composing a dictionary with 'elements': frequency pairs
+    frequency = Counter(elements)
+    #  From dictionary composing a list of tuples, sorted by frequency
+    sorted_frequency = frequency.most_common()
+    #  From that list getting only the keys
+    result = []
+    for key, value in sorted_frequency:
+        result.append(key)
+    return result
 
+
+def frequency_sort_string_with_counter_simplified(input_list: list):
+    """
+    Reduce the list to unique elements and sort them by frequency
+    >>> frequency_sort_string_with_counter_simplified(['a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c'])
+    ['b', 'c', 'a']
+    """
+    counter = Counter(input_list)
     return list(char for char, freq in counter.most_common())
 
 
@@ -36,29 +59,6 @@ def frequency_sorting_manual(elements: list):
             if frequency[key] == max(frequency.values()) and max(frequency.values()) != 0:
                 result.append(key)
                 frequency[key] = 0
-    return result
-
-
-def frequency_list_sorting_simplified(elements: list):
-    """
-    Reduce the list to unique elements and sort them by frequency
-    >>> frequency_list_sorting_simplified([1, 2, 1, 3, 3, 3, 3])
-    [3, 1, 2]
-
-    >>> frequency_list_sorting_simplified([1, 2, 1, 2, 3, 3])
-    [1, 2, 3]
-
-    >>> frequency_list_sorting_simplified(['c', 'c', 'b', 'b', 'b', 'a'])
-    ['b', 'c', 'a']
-    """
-    #  Composing a dictionary with 'elements': frequency pairs
-    frequency = Counter(elements)
-    #  From dictionary composing a list of tuples, sorted by frequency
-    sorted_frequency = frequency.most_common()
-    #  From that list getting only the keys
-    result = []
-    for key, value in sorted_frequency:
-        result.append(key)
     return result
 
 
