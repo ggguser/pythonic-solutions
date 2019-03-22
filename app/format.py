5358446806a35a5fa3f9657f285e686e379c98d6
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-22 10:07:03 +0300

    Formatting 1000000 as 1 000 000

--- app/format.py
+++
@@ -1,7 +0,0 @@
-def place_value(number):
-    return "{:,}".format(number)
-
-
-print(place_value(1000000))
-
-
---
+++ app/number_formatting.py
@@ -0,0 +1,6 @@
+def comma_separator(number):
+    return f"{number:,}"
+
+
+def space_separator(number):
+    return f"{number:_}".replace('_', ' ')
