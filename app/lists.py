2a86f1926df189247cc4806a77de7cd011f6a565
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-22 10:09:20 +0300

    Generating list of months

--- app/lists.py
+++ app/lists.py
@@ -11,3 +11,16 @@ def zipper(list_1, list_2):
     return zipped
 
 
+def list_of_months(start_month):
+    """
+    Make cycled list of months starting with given
+    >>> list_of_months(3)
+    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2]
+
+    >>> list_of_months(4)
+    [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3]
+    """
+    start_month -= 1  # human months start with 1 not 0
+    end_month = 12 + start_month
+    months = [((month % 12) + 1) for month in range(start_month, end_month)]
+    return months
