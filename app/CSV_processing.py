1804261d49c044f0d6b1765b270f7d15d4caae22
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-22 10:06:37 +0300

    Passing filename as argument

--- app/CSV_processing.py
+++ app/CSV_processing.py
@@ -1,41 +1,49 @@
 import csv
+from hashlib import md5
 
 
-def from_csv_to_dict():
+def from_csv_to_dict_with_tuples(filename):
+    """
+    from
+    key_name;9.999;8.777
+    to
+    {'key_name': (9.999, 8.777)}
+    """
+    with open(filename, encoding='utf-8', newline='') as csv_file:
+        delimiter = ';'  # what separates your values
+        csv_reader = csv.reader(csv_file, delimiter=delimiter)
+        #  Composing the dict through generator, converting values to appropriate types
+        imported_dict = {metro[0]: (float(metro[1]), float(metro[2])) for metro in csv_reader}
 
-    with open('metro.csv', newline='') as csv_file:
-        metro_import = csv.reader(csv_file, delimiter=';')
-        #  Composing the dict, converting values to appropriate types
-        metro_csv_import = {metro[0]: (float(metro[1]), float(metro[2])) for metro in metro_import}
-
-    return metro_csv_import
+    return imported_dict
 
 
-def count_number_of_entries_in_csv():
+def count_number_of_entries_in_csv(filename):
 
-    with open('realty.csv', newline='') as csv_file:
-        realty_reader = csv.reader(csv_file, delimiter=';')
-        row_count = sum(1 for row in realty_reader)
+    with open(filename, newline='', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        row_count = sum(1 for row in csv_reader)
 
     return row_count
 
 
-def get_hash_of_csv():
+def get_hash_of_csv(filename):
 
-    with open('realty.csv', 'rb') as realty_csv:
-        csv_reader = realty_csv.read()
-        csv_hash = hashlib.md5(csv_reader).hexdigest()
+    with open(filename, 'rb') as csv_file:
+        csv_reader = csv_file.read()
+        csv_hash = md5(csv_reader).hexdigest()
+
     return csv_hash
 
 
-def write_hash_of_csv(csv_hash):
+def write_hash_of_csv(filename, csv_hash):
 
-    with open('realty.csv.md5', 'w') as realty_hash_file:
+    with open(filename, 'w') as realty_hash_file:
         realty_hash_file.write(str(csv_hash))
 
 
-def read_hash_of_csv():
+def read_hash_of_csv(filename):
 
-    with open('realty.csv.md5') as csv_hash_file:
+    with open(filename) as csv_hash_file:
         csv_hash = csv_hash_file.read()
     return csv_hash
