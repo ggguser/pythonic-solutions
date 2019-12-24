bfd5675dd75927b45d41f202ae5de6b7d788a609
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-12-24 23:47:56 +0300

    Added tests

--- app/number_format.py
+++ app/number_format.py
@@ -1,6 +1,14 @@
-def comma_separator(number):
+def comma_separator(number) -> str:
+    """
+    >>> comma_separator(1000000)
+    '1,000,000'
+    """
     return f"{number:,}"
 
 
-def space_separator(number):
+def space_separator(number) -> str:
+    """
+    >>> space_separator(1000000)
+    '1 000 000'
+    """
     return f"{number:_}".replace('_', ' ')
